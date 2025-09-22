676936db6bc680ddfda2ed1ef6592513ac422829
Marvin SH <20221135@uthh.edu.mx>
2025-09-22 11:19:50 -0600

    add pantalla de resultados de busqueda

---
+++ lib/screens/results.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'search.dart';
+import 'reading.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ResultsScreen(),
+    );
+  }
+}
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // App Bar with Back Button and Title
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+              color: Colors.green,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.arrow_back, color: Colors.black),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SearchScreen()),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Resultados de busqueda',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Buscar libros...',
+                      prefixIcon: Icon(Icons.search, color: Colors.grey),
+                      suffixIcon: Icon(Icons.mic, color: Colors.grey),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Search Results
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.book),
+                    title: Text('Libro 1'),
+                    tileColor: Colors.grey[200],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ReadingScreen()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
-// import 'results.dart';
+import 'results.dart';
 // import 'saved.dart';
 
 void main() {
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen ({super.key});
+  const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +48,9 @@ class SearchScreen extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => LoginScreen()),
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
                         );
                       },
                     ),
@@ -89,10 +91,10 @@ class SearchScreen extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: 16.0),
               child: ElevatedButton(
                 onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(builder: (context) => ResultsScreen()),
-                  // );
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResultsScreen()),
+                  );
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.orange,
@@ -106,7 +108,7 @@ class SearchScreen extends StatelessWidget {
                   'Ver todo',
                   style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
-            ),
+              ),
             ),
             Spacer(),
             // Libros Guardados Button
