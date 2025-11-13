9a450ef12893a8c95ef4f2930501cf3dd914d6d1
20221039@uthh.edu.mx <20221039@uthh.edu.mx>
2025-11-12 22:15:36 -0600

    feat: update app branding with new logo and name
    - Add new logo.png and update launcher icons for all Android densities
    - Update app name in main.dart and AndroidManifest
    - Add flutter_launcher_icons package to generate icons

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CRESPF Biblioteca Digital',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
